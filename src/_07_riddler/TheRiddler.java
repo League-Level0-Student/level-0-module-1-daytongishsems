6cd305fb1aad609dc61df5295c7482be220f0990
Dayton Gish <Gish.Dayton@SEMS259.LAB>
2019-12-18 16:27:15 -0800

    The last recipe I finished was making the robot go downwards.

--- src/_07_riddler/TheRiddler.java
+++ src/_07_riddler/TheRiddler.java
@@ -2,7 +2,9 @@ package _07_riddler;
 //    Copyright (c) The League of Amazing Programmers 2013-2019
 //    Level 0
 
+import java.util.Random;
 
+import javax.swing.JOptionPane;
 
 public class TheRiddler {
 
@@ -19,7 +21,24 @@ public class TheRiddler {
 		// 6. Add some more riddles
 
 		// 2. Make a pop up to show the score.
+	
+		int score = 0;	
+		 String talk0 = JOptionPane.showInputDialog("What is greater than God,"+
+				 " more evil than the devil,"+
+				 "the poor have it,"+
+				 "the rich need it,"+
+				 "and if you eat it, you'll die?");
+		
+		 if (talk0.equals("Nothing")) {
+			 score = score +1;
+			  JOptionPane.showInputDialog("+1 Point");
+		 }
+			
+		 else {
+				  JOptionPane.showMessageDialog(null, "You're Wrong, the correct answer is Nothing.");
+		
+		
+		 }	
 		
 	}
-}
-
+ }
--- src/_08_robot_in_space/RobotInSpace.java
+++ src/_08_robot_in_space/RobotInSpace.java
@@ -28,11 +28,16 @@ public class RobotInSpace implements KeyEventDispatcher {
 	private void moveRobot(int keyPressed) throws InterruptedException {
 		// 2. Print out the keyPressed variable and write down the numbers for
 		// each arrow key
+		System.out.println(keyPressed);
 
 		// 3. If the up arrow is pressed, move the Robot up the screen.
-
+if(keyPressed==38) {
+	rob.setAngle(0);
+}
 		// 4. If the down arrow is pressed, move the Robot down.
-
+if(keyPressed==40) {
+	rob.setAngle(180);
+}
 		// 5. If the left arrow is pressed, make the Robot go left.
 
 		// 6. If right is pressed, move the Robot right.
@@ -86,3 +91,5 @@ public class RobotInSpace implements KeyEventDispatcher {
 		}
 	}
 }
+
+
