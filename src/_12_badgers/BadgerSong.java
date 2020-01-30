d0eec80059474e5b452d58606d97859e6ff35e8f
Dayton Gish <Gish.Dayton@SEMS259.LAB>
2020-01-29 16:21:31 -0800

    This is what i worked on today
    This is my code i did

---
+++ src/_12_badgers/BadgerSong.java
@@ -0,0 +1,46 @@
+package _12_badgers;
+
+import javax.swing.JOptionPane;
+
+public class BadgerSong {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	
+		
+		for (int i = 0; i < 8; i++) {
+			System.out.println("Badger");	
+			
+		}
+		
+		for (int i = 0; i < 2; i++) {
+		System.out.println("Mushroom");	
+		}
+	
+	for (int i = 0; i < 2; i++) {
+		System.out.println("A Snake");	
+	}
+
+	for (int i = 0; i < 2; i++) {
+		System.out.println("Snaaake");	
+	}
+	for (int i = 0; i < 1; i++) {
+		System.out.println("Oooh, It's A Snake");
+	}
+	}
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
+
---
+++ src/_13_bananas_4eva/Bannas4Eva.java
@@ -0,0 +1,19 @@
+package _13_bananas_4eva;
+
+public class Bannas4Eva {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+		for (int i = 0; i < 1000; i++) {
+			System.out.println("banana");		
+		
+		
+		}
+		
+		
+		
+	}
+
+}
