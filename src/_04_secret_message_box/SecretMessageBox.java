0a6d365759430f35085d53427fddb23108debaae
Dayton Gish <Gish.Dayton@SEMS259.LAB>
2020-02-19 16:26:54 -0800

    hdfhdhdh

--- src/_04_secret_message_box/SecretMessageBox.java
+++ src/_04_secret_message_box/SecretMessageBox.java
@@ -17,7 +17,7 @@ import javax.swing.JOptionPane;
 public class SecretMessageBox {
 
 	// 0. Make a main method and put steps 1-5 inside it
-	public static void main(String[] args) {
+
 		
 	
 	// 1. Set a password in a String variable
